d5d7d3211fb264da7139330eb4154b13823e682e
Rhytim <marclm.mao@gmail.com>
2020-03-11 16:22:43 +0100

    pato al  hacer click cambia de posicion

--- app/src/main/java/com/example/cazapatos/GameActivity.java
+++ app/src/main/java/com/example/cazapatos/GameActivity.java
@@ -3,19 +3,27 @@ package com.example.cazapatos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.cazapatos.common.Constantes;
 
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvCounterPatos, tvTimer, tvNick;
     ImageView ivPato;
     int counter = 0;
+    int anchoPantalla;
+    int altoPantalla;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +49,54 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         //listeners
         ivPato.setOnClickListener(this);
+
+        //cogemos la grandaria de la pantalla del dispositivo donde ejecutaremos el juego
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        anchoPantalla = size.x; //x por coordenada x izquierdaderecha
+        altoPantalla = size.y; //y por la coordenada y arribaabajo
+
+        //inicializamos el objeto random
+        random = new Random();
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.imageViewPato: //sube el contador de numero de veces hecho click sobre el pato
+            case R.id.imageViewPato:
+                //sube el contador de numero de veces hecho click sobre el pato
                 counter++;
                 tvCounterPatos.setText(String.valueOf(counter));
+
+                //cambia la imagen del pato al ser clicado y la restaura en medio segundo
+                ivPato.setImageResource(R.drawable.duck_clicked);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ivPato.setImageResource(R.drawable.duck);
+                        moverPato();
+                    }
+                }, 500);
+
                 break;
         }
+    }
+
+    public void moverPato(){
+        int min = 0; //(0,0)
+        int maxX = anchoPantalla - ivPato.getWidth(); //restandole la anchura del pato es para que no se salga
+        //de la pantalla su cuerpo
+        int maxY = altoPantalla - ivPato.getHeight();//restandole la altura del pato es para que no se salga
+        //de la pantalla
+
+        //generamos dos numeros aleatorios, para la coordenada x y.
+        int randomX = random.nextInt(((maxX - min)+1)+min);
+        int randomY = random.nextInt(((maxY - min)+1)+min);
+
+        //utilizamos los randoms para mover el pato
+        ivPato.setX(randomX);
+        ivPato.setY(randomY);
     }
 }
