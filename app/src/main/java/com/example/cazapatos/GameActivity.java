192bc946699e00477757e61bf428a5b5910c524b
Rhytim <marclm.mao@gmail.com>
2020-03-12 14:20:37 +0100

    al acabarse la partida i el tiempo muestra un dialogo donde podemos salir de la actividad o bien reiniciar la partida

--- app/src/main/java/com/example/cazapatos/GameActivity.java
+++ app/src/main/java/com/example/cazapatos/GameActivity.java
@@ -1,7 +1,9 @@
 package com.example.cazapatos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.Typeface;
@@ -25,6 +27,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     int anchoPantalla;
     int altoPantalla;
     Random random;
+    boolean acabado = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,46 +64,38 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         //inicializamos el objeto random
         random = new Random();
 
+        moverPato();
+
         //cuenta atras
-        new CountDownTimer(60000, 1000) { //param1: recibe los milisegundos que va a durar param2: milisegundos que va a ir bajando
-
-            //se llama cada segundo que pasa
-            public void onTick(long millisUntilFinished) {
-                long segundosRestantes = millisUntilFinished / 1000;
-                tvTimer.setText(segundosRestantes+"s");
-            }
-
-            //cuando acaba el tiempo
-            public void onFinish() {
-                tvTimer.setText("0s");
-            }
-        }.start();
+        cuentaAtras();
     }
-
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.imageViewPato:
-                //sube el contador de numero de veces hecho click sobre el pato
-                counter++;
-                tvCounterPatos.setText(String.valueOf(counter));
+                //si la partida no ha acabado aun, se puede hacer click al pato
+                if (!acabado) {
+                    //sube el contador de numero de veces hecho click sobre el pato
+                    counter++;
+                    tvCounterPatos.setText(String.valueOf(counter));
 
-                //cambia la imagen del pato al ser clicado y la restaura en medio segundo
-                ivPato.setImageResource(R.drawable.duck_clicked);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        ivPato.setImageResource(R.drawable.duck);
-                        moverPato();
-                    }
-                }, 500);
+                    //cambia la imagen del pato al ser clicado y la restaura en medio segundo
+                    ivPato.setImageResource(R.drawable.duck_clicked);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            ivPato.setImageResource(R.drawable.duck);
+                            moverPato();
+                        }
+                    }, 500);
+                }
 
                 break;
         }
     }
 
-    public void moverPato(){
+    public void moverPato() {
         int min = 0; //(0,0)
         int maxX = anchoPantalla - ivPato.getWidth(); //restandole la anchura del pato es para que no se salga
         //de la pantalla su cuerpo
@@ -108,11 +103,55 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         //de la pantalla
 
         //generamos dos numeros aleatorios, para la coordenada x y.
-        int randomX = random.nextInt(((maxX - min)+1)+min);
-        int randomY = random.nextInt(((maxY - min)+1)+min);
+        int randomX = random.nextInt(((maxX - min) + 1) + min);
+        int randomY = random.nextInt(((maxY - min) + 1) + min);
 
         //utilizamos los randoms para mover el pato
         ivPato.setX(randomX);
         ivPato.setY(randomY);
+    }
+
+    private void cuentaAtras(){
+        new CountDownTimer(10000, 1000) { //param1: recibe los milisegundos que va a durar param2: milisegundos que va a ir bajando
+
+            //se llama cada segundo que pasa
+            public void onTick(long millisUntilFinished) {
+                long segundosRestantes = millisUntilFinished / 1000;
+                tvTimer.setText(segundosRestantes + "s");
+            }
+
+            //cuando acaba el tiempo
+            public void onFinish() {
+                tvTimer.setText("0s");
+                acabado = true;
+                mostrarCuadroDialogoAcabado();
+            }
+        }.start();
+    }
+
+    //el usuario vera los patos que ha cazado y ademas vamos a gestionar la alternativa de salir del juego o reiniciar la partida
+    private void mostrarCuadroDialogoAcabado() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+        builder.setMessage("Has conseguido cazar " + counter + " patos")
+                .setTitle("Game Over");
+
+        builder.setPositiveButton("Reiniciar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                counter = 0;
+                tvCounterPatos.setText("0");
+                acabado = false;
+                cuentaAtras();
+                moverPato();
+            }
+        });
+        builder.setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
