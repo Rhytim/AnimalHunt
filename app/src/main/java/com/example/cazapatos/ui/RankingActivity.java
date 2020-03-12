781bfc88e67e2ea3df70ce4977f9e9d6cdb796d0
Rhytim <marclm.mao@gmail.com>
2020-03-12 21:12:49 +0100

    eliminado toolbar i modificado boton flotante de la activitat ranking

--- app/src/main/java/com/example/cazapatos/ui/RankingActivity.java
+++ app/src/main/java/com/example/cazapatos/ui/RankingActivity.java
@@ -1,5 +1,6 @@
 package com.example.cazapatos.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,20 +19,22 @@ public class RankingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Intent i=new Intent(RankingActivity.this,LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // a b c d, elimina c i d, i obre l'activitat b
+                startActivity(i);
+                //finish();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, new UserRankingFragment()).commit();
     }
-
 }
